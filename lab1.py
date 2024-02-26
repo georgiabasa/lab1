a034941e9cc08e7110674afee7e304f973a2a2b6
georgiabasa <up1084016@ac.upatras.gr>
2024-02-26 13:37:15 +0200

    #expiration date idi printed

--- lab1.py
+++ lab1.py
@@ -11,17 +11,24 @@ def more(text):
             if reply == 'n':
                 break
 
-url=input("Enter a URL: ")
+#zitaei aitima apo ton xristi
+url=input("Enter a URL: ") 
 
+#http aitima
 if not url.startswith("http://"):
     url = "http://" + url
 
+#pragmatopoiei aitima get se afto to url http
 with requests.get(url) as response:  # το αντικείμενο response
     #html = response.text
     #more(html)
 
-    print(f"Website headers are {url} \n, {response.headers} \n\n")
+    #tupwnei headers apokrisis response http
+    print("\nRESPONSE HEADER")
+    for key, value in response.headers.items():
+        print(f"{key:30s} {value}")
 
+    #(a) poio logismiko xrisimopoiei o web server
     server=response.headers.get("Server")
 
     if server:
@@ -29,11 +36,14 @@ with requests.get(url) as response:  # το αντικείμενο response
     else:
         print("No server found")
 
+    #(b) an xrisimopoiei cookies
     cookies = response.headers.get("Set-Cookie")
 
     if cookies:
+        print(f"The webpage is using cookies")
         cookies=cookies.split(';')
         for cookie in cookies:
+            #onoma ka8e cookie kai expiration date se opoio exei
             print(f"The cookie is {cookie}")
     else:
         print("No cookie found")
